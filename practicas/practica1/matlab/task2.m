5ba70d1d23023240ec9ce5f91c4f6648b367f302
bedrix19 <renatojosue@hotmail.es>
2025-10-23 23:34:27 +0200

    nalñsdfouihasdñtgfhawsdtfasd
    c:

--- practicas/practica1/matlab/task2.m
+++ practicas/practica1/matlab/task2.m
@@ -3,49 +3,28 @@
 FS = 5;
 N = 10;
 
-% (0.5*FS, 0.75*FS, FS, 1.03*FS)
-A1 = 0.5*FS;
-A2 = 0.75*FS;
-A3 = FS;
-A4 = 1.03*FS;
-
-f0 = 18.17e6; % in Hz
+f0 = 18.17e6;
 numSamples = 15 * 1024;
 
-fs = 100e6; % in Hz
+fs = 100e6;
 t = (0:numSamples-1) / fs;
 
-x_t1 = A1*cos(2*pi*f0*t);
-x_t2 = A2*cos(2*pi*f0*t);
-x_t3 = A3*cos(2*pi*f0*t);
-x_t4 = A4*cos(2*pi*f0*t);
-
-xq1 = quanti(x_t1,FS,N);
-xq2 = quanti(x_t2,FS,N);
-xq3 = quanti(x_t3,FS,N);
-xq4 = quanti(x_t4,FS,N);
-
+x_t = FS*cos(2*pi*f0*t);
+xq = quanti(x_t, FS, N);
+e = x_t - xq;
 figure;
-%hist(x_t-xq,40);
-histogram(x_t1-xq1,40);
+histogram(e,40);
 xlabel('Error');
 ylabel('Frequency');
-title('Histogram of Quantization Error for A=0.5*FS');
+title('Histogram of Quantization Error');
 
-figure;
-histogram(x_t2-xq2,40);
-xlabel('Error');
-ylabel('Frequency');
-title('Histogram of Quantization Error for A=0.75*FS');
+var_e_emp = var(e,1);
+var_e_theor = (FS / 2^(N-1))^2 / 12;
+fprintf('Empirical var(e) = %g; theoretical var(e) = %g\n', var_e_emp, var_e_theor);
 
-figure;
-histogram(x_t3-xq3,40);
-xlabel('Error');
-ylabel('Frequency');
-title('Histogram of Quantization Error for A=FS');
+sigma_x = sqrt(var(x,1)); % or FS / sqrt(2)
 
-figure;
-histogram(x_t4-xq4,40);
-xlabel('Error');
-ylabel('Frequency');
-title('Histogram of Quantization Error for A=1.03*FS');
+var_x_emp = var(x,1);
+SQNR_emp = 10*log10(var_x_emp/var_e_emp);
+SQNR_theor = 6.02*N+4.77 - 20 * log10(FS / sigma_x);
+fprintf('SQNR_emp = %.4f dB; SQNR_theor = %.4f dB\n', SQNR_emp, SQNR_theor);
